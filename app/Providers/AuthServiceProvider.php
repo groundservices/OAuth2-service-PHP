ce424a8a4beb8cd98a4e68e0ca1a71eb83112d04
Lyndon <mcjohnsonlyndon@gmail.com>
2020-09-21 22:24:23 +0000

    passport Authentication configuration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,13 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::personalAccessClientId(
+            config('passport.personal_access_client.id')
+        );
+
+        Passport::personalAccessClientSecret(
+            config('passport.personal_access_client.secret')
+        );
     }
 }
