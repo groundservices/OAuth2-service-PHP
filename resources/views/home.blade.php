0c89557594e9cc59cb606de9c76884ec52d43ccd
Lyndon <mcjohnsonlyndon@gmail.com>
2020-09-21 23:06:52 +0000

    passport UI setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <h2 class="font-weight-bold">{{ __('Dashboard') }}</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+            <passport-clients class="my-4"></passport-clients>
+            <passport-authorized-clients class="my-4"></passport-authorized-clients>
+            <passport-personal-access-tokens class="my-4"></passport-personal-access-tokens>
         </div>
     </div>
 </div>
